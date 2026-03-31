7cfe1980e4e22da98c7148255e2c5fb4dd430f4d
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2026-03-30 20:15:52 -0400

    f62: Project Matrixx bring-up

--- matrixx_f62.mk
+++ matrixx_f62.mk
@@ -28,7 +28,7 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/matrixx/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_f62
